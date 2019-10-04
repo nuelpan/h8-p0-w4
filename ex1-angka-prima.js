c72ade7829f4ad00cc970f1d0caf9300e62f4239
Immanuel Pandiangan <nuelpan@gmail.com>
2019-10-04 12:42:55 +0700

    add test case in ex-1, add ex2

--- ex1-angka-prima.js
+++ ex1-angka-prima.js
@@ -1,4 +1,4 @@
-function isItPrime(num) {
+function angkaPrima(num) {
   if (num === 1) {
     return false;
   } else if (num === 2) {
@@ -6,13 +6,16 @@ function isItPrime(num) {
   } else if (num > 2) {
     for (var i = 2; i < num; i++) {
       if (num % i === 0) {
-        return "bukan prima";
+        return "false";
       }
     }
-    return "prima";
+    return "true";
   }
 }
 
-console.log(isItPrime(14));
-console.log(isItPrime(3));
-console.log(isItPrime(140));
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
