e669abe7065f50672e6cf7cbe6cfbbe5253070c2
Immanuel Pandiangan <nuelpan@gmail.com>
2019-10-11 13:27:14 +0700

    update exercise-9

--- ex-9-check-AB.js
+++ ex-9-check-AB.js
@@ -1,9 +1,11 @@
 function checkAB(num) {
   // you can only write your code here!
-  // cek kedepan kalau ada lawan main
   var temp;
   for (var i = 0; i < num.length; i++) {
-    if (num[i] === "a" && (num[i - 4] === "b" || num[i + 4] === "b")) {
+    if (
+      num[i] === "a" &&
+      (num[i + 4] === "b" || (num[i] === "b" && num[i + 4] === "a"))
+    ) {
       return true;
     } else {
       temp = false;
